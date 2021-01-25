df6bcc8af9175948b0405796a34e6f7bc72d6a0b
iromli <isman.firmansyah@gmail.com>
2021-01-26 04:03:45 +0700

    add client-api

---
+++ client-api/deploy-pod.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+NGINX_IP=${NGINX_IP:-$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')}
+
+cat client-api.yml | sed -s "s@NGINX_IP@$NGINX_IP@g" | kubectl apply -f -
