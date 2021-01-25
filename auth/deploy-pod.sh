0fcd5d3b30aceeb14dd6d15a6e5eb4cf79da2686
iromli <isman.firmansyah@gmail.com>
2021-01-26 03:05:17 +0700

    auth: change yaml to yml

--- auth/deploy-pod.sh
+++ auth/deploy-pod.sh
@@ -2,4 +2,4 @@
 
 NGINX_IP=${NGINX_IP:-$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')}
 
-cat auth.yaml | sed -s "s@NGINX_IP@$NGINX_IP@g" | kubectl apply -f -
+cat auth.yml | sed -s "s@NGINX_IP@$NGINX_IP@g" | kubectl apply -f -
